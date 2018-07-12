858b11fd636fa877b602e3b80644c84f91da30c7
Uros Marinkovic <marinko29@live.com>
2018-07-12 14:47:48 +0200

    knjizenjeRobePocetak

--- OSPjsFajlovi/knjizenjeRobe.js
+++ OSPjsFajlovi/knjizenjeRobe.js
@@ -16,14 +16,57 @@ function skloniModalKnjizenjeRobe() {
         odBlokirajDugmice("dugmeRokovnik","dugmeKalendar","dugmeKontakt");
     }
 }
-let dugmeDodajProizvod=document.getElementById('dugmeDodajProizvod'),
-    knjizenjeRobeForma=document.getElementById('knjizenjeRobeForma'),
-    brojProizvoda=1;
-function knjizenjeRobeNapraviInput() {
+let dugmeDodajProizvod=document.getElementById('dugmeDodajProizvod');
+proizvodi={};
+function sacuvajProizvodULocalStorage() {
     dugmeDodajProizvod.addEventListener('click',(event)=>{
         event.preventDefault();
-        knjizenjeRobeForma.innerHTML +=`  <label for="nazivProizvoda">Naziv:</label> <input type="text" name="nazivProizvoda" id="nazivProizvoda">  <label for="brojProizvod">Kolicina:</label><input type="number" name="brojProizvod" id="brojProizvod${brojProizvoda}"><br>`;
-        ++brojProizvoda;
+        let sifraProizvoda=document.getElementById('sifraProizvoda').value,
+            nazivProizvoda=document.getElementById('nazivProizvoda').value,
+            kolicinaProizvoda=document.getElementById('kolicinaProizvoda').value,
+            opisProizvoda=document.getElementById('opisProizvod').value,
+            proizvodi=localStorage.getItem("knjizenjeProizvoda") !== null ? JSON.parse(localStorage.getItem("knjizenjeProizvoda")) : {};
+            if (sifraProizvoda==="") {
+                alert('Šifra proizvoda je obavezna !')
+                return;
+            }
+            if (nazivProizvoda==="") {
+                alert('Naziv proizvoda je obavezan !')
+                return;
+            }
+        let noviProizvodi={
+            sifra: sifraProizvoda,
+            naziv: nazivProizvoda,
+            kolicina:kolicinaProizvoda,
+            opis: opisProizvoda
+        };
+
+        proizvodi[sifraProizvoda]=noviProizvodi
+        setToLocalStorage('knjizenjeProizvoda',proizvodi);
+        alert('Uspešno ste dodali novi proizvod');
+        prikazSacuvanihProizvoda();
     })
+   
 }
-knjizenjeRobeNapraviInput();
+sacuvajProizvodULocalStorage();
+
+function prikazSacuvanihProizvoda() {
+    let prikazIzFormeKnjizenje=document.getElementById('prikazIzFormeKnjizenje'),
+        robaIzLocalStorage=localStorage.getItem('knjizenjeProizvoda');
+        if (robaIzLocalStorage==null) {
+            proizvodi={}
+        }else{
+            proizvodi=JSON.parse(robaIzLocalStorage);
+            console.log(proizvodi);
+        }
+        let knjizenjeRobeStavkeHTML = '<ul id="lista">';
+        for (let kljucKnjizenjeRobe in proizvodi) {
+            knjizenjeRobeStavkeHTML += `<li id="knjizenjeRobestavkaID${kljucKnjizenjeRobe}">${kljucKnjizenjeRobe}<em id="dugmeIzbrisiID${kljucKnjizenjeRobe}" onclick="">izbrisi</em><em id="dugmeIzmeniID${kljucKnjizenjeRobe}" onclick="">izmeni</em></li>`;  
+           
+           
+        }
+        knjizenjeRobeStavkeHTML += '</ul>';
+        prikazIzFormeKnjizenje.innerHTML=knjizenjeRobeStavkeHTML;
+    
+}
+prikazSacuvanihProizvoda();
